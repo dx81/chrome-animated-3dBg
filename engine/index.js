3ffc5affc341925e75f3c64211b989cf825242a3
Ulrich Barnstedt <ulrich.barnstedt+git@gmail.com>
2021-03-02 19:00:19 +0100

    Initial page creation, CSS fill whole page

---
+++ engine/index.js
@@ -0,0 +1,6 @@
+window.onload = () => {
+    let ctx = document.getElementById("main").getContext("2d");
+
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
+}

fa817235893386dd0b7d0b7ac4ed261c5529e775
Ulrich Barnstedt <ulrich.barnstedt+git@gmail.com>
2021-03-06 20:46:44 +0100

    Implemented UI logic, setup chrome storage and user JSON manipulation.

--- engine/index.js
+++ engine/index.js
@@ -1,7 +1,21 @@
-//temp code to test css styling
-window.onload = () => {
-    let ctx = document.getElementById("main").getContext("2d");
+import defaultJSON from "./defaultJSON.js"
 
+window.onload = () => {
+    chrome.storage.sync.get("sceneData", (res) => {
+        if (Object.keys(res).length === 0) {
+            ready(defaultJSON);
+            return;
+        }
+
+        ready(res.sceneData);
+    })
+
+
+    let ctx = document.getElementById("main").getContext("2d");
     ctx.fillStyle = "#000000";
     ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
+}
+
+const ready = (json) => {
+    console.log(json);
 }
--- page/menu.js
+++ page/menu.js
@@ -1,7 +1,21 @@
+const mapFn = (obj) => {
+    for (let key in obj) {
+        document.getElementById(key).addEventListener(obj[key][0], obj[key][1]);
+    }
+}
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById("openButton").addEventListener("click", menu.open);
-    document.getElementById("closeButton").addEventListener("click", menu.close);
+    mapFn({
+        openButton : ["click", menu.open],
+        closeButton : ["click", menu.close],
+
+        uploadButton : ["change", JSONManagement.uploadHandler],
+        clearButton : ["click", JSONManagement.clearHandler]
+    });
+
+    JSONManagement.setupDownload();
 });
+
+//---------------- actual functions
 
 const menu = {
     open : () => {
@@ -12,3 +26,45 @@ const menu = {
     }
 }
 
+const JSONManagement = {
+    uploadHandler : function () {
+        let file = this.files[this.files.length - 1];
+
+        if (file.type !== "application/json") {
+            alert("Only json files are supported. This file has the type " + file.type);
+            return;
+        }
+
+        const fileReader = new FileReader();
+        fileReader.addEventListener("load", JSONManagement.processFile);
+
+        fileReader.readAsText(file);
+    },
+    clearHandler : () => {
+        chrome.storage.sync.remove("sceneData", () => {
+            location.reload();
+        })
+    },
+    processFile : (data) => {
+        data = data.target.result;
+        let jsonData;
+
+        try {
+            jsonData = JSON.parse(data);
+        } catch (e) {
+            alert(`This file is not valid JSON.\nError: ${e}`);
+        }
+
+        console.log(jsonData);
+        chrome.storage.sync.set({sceneData : jsonData}, () => {
+            location.reload();
+        });
+    },
+    setupDownload : () => {
+        chrome.storage.sync.get("sceneData", res => {
+            if ("sceneData" in res) res = res.sceneData;
+
+            document.getElementById("downloadButtonWrapper").setAttribute("href", `data:text/plain;charset=utf-8,${encodeURIComponent(JSON.stringify(res))}`);
+        })
+    }
+}

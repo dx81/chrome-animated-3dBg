37e70b13ac4e37ba96a6a764e6282d8c0531c606
OliverKovacs <oliver.kovacs.dev@gmail.com>
2021-03-12 13:02:00 +0100

    Added web2 scene

--- engine/js/scenes/scenes.js
+++ engine/js/scenes/scenes.js
@@ -5,6 +5,7 @@ import d20 from "./d20.js";
 import d12d20 from "./d12d20.js";
 import satan from "./satan.js";
 import web from "./web.js";
+import web2 from "./web2.js";
 import faces from "./faces.js";
 import test from "./test.js";
 import empty from "./empty.js";
@@ -17,6 +18,7 @@ export default {
     d12d20,
     satan,
     web,
+    web2,
     faces,
     test,
     empty,
